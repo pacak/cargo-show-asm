db8dde232a5a1e09c7cfa0496a9b15caabf7bd01
Anshul Gupta <ansg191@anshulg.com>
2023-11-08 20:38:02 -0800

    Adds support for `no_mangle` in macOS cdylib

--- sample_cdylib/src/lib.rs
+++ sample_cdylib/src/lib.rs
@@ -2,3 +2,13 @@
 pub extern "C" fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+#[no_mangle]
+pub extern "C" fn sub(left: usize, right: usize) -> usize {
+    left - right
+}
+
+#[no_mangle]
+pub extern "C" fn _mul(left: usize, right: usize) -> usize {
+    left * right
+}
